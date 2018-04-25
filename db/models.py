b523ec42763b99b632eb41601f9c6c9d5916c376
dornheimer <beaveroftime@gmail.com>
2018-04-25 15:00:48 +0200

    remove index for genres.name, remove redundant index for lines.id

--- db/models.py
+++ db/models.py
@@ -53,7 +53,7 @@ class Movie(Base):
     title = Column(String)
     year = Column(String)
 
-    __table_args__ = (Index('movie_ix', 'id', 'title', unique=True), {})
+    __table_args__ = (Index('ix_movies_id_movie_title', 'id', 'title', unique=True), {})
 
     characters = relationship('Character', back_populates='movie')
     conversations = relationship('Conversation', back_populates='movie')
@@ -76,7 +76,7 @@ class Genre(Base):
     __tablename__ = 'genres'
     id = Column(Integer, primary_key=True)
 
-    name = Column(String, index=True)
+    name = Column(String)
 
     movies = relationship('Movie', secondary=movies_genres, back_populates='genres')
 
@@ -96,7 +96,7 @@ class Character(Base):
 
     __table_args__ = (
         ForeignKeyConstraint([movie_id, movie_title], [Movie.id, Movie.title]),
-        Index('character_ix', 'id', 'name', unique=True),
+        Index('ix_character_id_character_name', 'id', 'name', unique=True),
         {}
     )
 
@@ -123,7 +123,7 @@ class Character(Base):
 
 class Line(Base):
     __tablename__ = 'lines'
-    id = Column(String, primary_key=True, index=True)
+    id = Column(String, primary_key=True)
 
     character_id = Column(String)
     character_name = Column(String)
@@ -156,13 +156,13 @@ class Conversation(Base):
     __tablename__ = 'conversations'
     id = Column(Integer, primary_key=True)
 
-    first_char_id = Column(String, ForeignKey(
-        'characters.id', onupdate='CASCADE', ondelete='SET NULL'
-        )
+    first_char_id = Column(
+        String,
+        ForeignKey('characters.id', onupdate='CASCADE', ondelete='SET NULL')
     )
-    second_char_id = Column(String, ForeignKey(
-        'characters.id', onupdate='CASCADE', ondelete='SET NULL'
-        )
+    second_char_id = Column(
+        String,
+        ForeignKey('characters.id', onupdate='CASCADE', ondelete='SET NULL')
     )
     movie_id = Column(String, ForeignKey('movies.id'))
 
