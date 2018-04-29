b47dfc4687fdd55baedad36495c7342a490f440c
dornheimer <beaveroftime@gmail.com>
2018-04-29 10:23:52 +0200

    remove fallback database URI

--- config.py
+++ config.py
@@ -7,9 +7,7 @@ load_dotenv(os.path.join(APP_DIR, '.env'))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URI', 'sqlite:///' + os.path.join(APP_DIR, 'movie_dialogs.sqlite')
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     TESTING = False
