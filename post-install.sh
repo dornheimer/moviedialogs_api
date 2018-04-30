fdaf505441491ca88ed79a34e4bb886a5c971cf6
dornheimer <beaveroftime@gmail.com>
2018-04-30 12:52:47 +0200

    implement postinstall hook for setuptools, remove postinstall.sh

--- post-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CORPUS_URL="http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip"
-wget -nc $CORPUS_URL
-unzip -nj ${CORPUS_URL##*/} -d corpus
-alembic upgrade head
-python -m db.seed
--- setup.py
+++ setup.py
@@ -1,7 +1,43 @@
-from setuptools import setup, find_packages
+import os
+import sys
+
+from setuptools import setup, find_packages, Command
+
+CORPUS_URL = 'http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip'
+
+
+class PostInstallCommand(Command):
+    """Support setup.py postinstall."""
+
+    description = 'Download the corpus data and create the database.'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print('Downloading corpus ...')
+        os.system(f'wget -nc {CORPUS_URL}')
+
+        print('Extracting ...')
+        os.system('unzip -nj {} -d corpus'.format(os.path.basename(CORPUS_URL)))
+
+        print('Creating database tables ...')
+        os.system('flask db upgrade')
+
+        print('Inserting data ...')
+        os.system('{} -m db.seed'.format(sys.executable))
+
+        sys.exit()
 
 setup(
     version=0.1,
     name='api',
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests',)),
+    cmdclass={
+        'postinstall': PostInstallCommand,
+    },
 )
